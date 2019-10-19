ce2140686ae9ff912e2cf022e5893c3e6e99bc18
Stolen <stolenqwerty@gmail.com>
2019-10-19 15:52:16 +0700

    db setup

--- config/db.php
+++ config/db.php
@@ -1,14 +1,11 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+use yii\db\Connection;
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+return [
+    'class' => Connection::class,
+    'dsn' => "mysql:host={$_ENV['MYSQL_HOST']};dbname={$_ENV['MYSQL_DATABASE']}",
+    'username' => $_ENV['MYSQL_USER'],
+    'password' => $_ENV['MYSQL_PASSWORD'],
+    'charset' => 'utf8',
 ];
